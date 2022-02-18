c652a84df1eba82b9362743dbd502b0b0b3d5036
jkawamura <jkawamura@haverford.edu>
2022-02-18 15:43:14 -0500

    Added inventory_material

--- escalateclient/core/chemistry.py
+++ escalateclient/core/chemistry.py
@@ -10,3 +10,17 @@ class ChemistryAPIMixin:
             search (dict, optional): [description]. Defaults to {}.
         """
         ...
+        endpoint = f"{Endpoints.INVENTORY_MATERIAL.value}"
+        if len(search)>0:
+            query_parameters_list = [
+            f"{q}={p}" if not p.startswith("!") else f"{q}!={p[1:]}"
+            for q,p in search.items()
+            ]
+
+            query_parameters = '&'.join(query_parameters_list)
+            endpoint = f"{endpoint}?{query_parameters}"
+        
+        materials = self.get(endpoint=endpoint)
+        return materials
+
+
--- escalateclient/core/constants.py
+++ escalateclient/core/constants.py
@@ -4,3 +4,5 @@ from enum import Enum
 class Endpoints(Enum):
     EXPERIMENT_TEMPLATE = "experiment-template"
     EXPERIMENT_INSTANCE = "experiment-instance"
+    INVENTORY_MATERIAL = "inventory-material"
+    
