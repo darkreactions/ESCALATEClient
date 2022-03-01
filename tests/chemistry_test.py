96c5451acfa4f467e3815b02870d80357ef724ab
jkawamura <jkawamura@haverford.edu>
2022-03-01 00:08:48 -0500

    Added tests

--- tests/chemistry_test.py
+++ tests/chemistry_test.py
@@ -91,4 +91,97 @@ def test_get_icontains_case(client):
         if "iodide" not in material["description"]:
             assert False
     assert True
-    
+
+def test_get_in(client):
+    search={
+        "description__in":"2-Pyrrolidin-1-ium-1-ylethylammonium iodide,"
+        }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if "2-Pyrrolidin-1-ium-1-ylethylammonium iodide" not in material["description"]:
+            assert False 
+    assert True 
+
+def test_greater_than(client):
+    search={
+        "onhand_amt__gt":"2.0"
+    }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if material["onhand_amt"]["value"] <= 2.0:
+            assert False 
+    assert True 
+
+def test_greater_than_equals(client):
+    search={
+        "onhand_amt__gte":"2.0"
+    }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if material["onhand_amt"]["value"] < 2.0:
+            assert False 
+    assert True 
+
+def test_less_than(client):
+    search={
+        "onhand_amt__lt":"2.0"
+    }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if material["onhand_amt"]["value"] >= 2.0:
+            assert False 
+    assert True 
+
+def test_less_than_equal(client):
+    search={
+        "onhand_amt__lte":"2.0"
+    }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if material["onhand_amt"]["value"] >= 2.0:
+            assert False 
+    assert True 
+
+def test_get_startwith(client):
+    search={
+        "description__startswith": "4-Fluoro"
+        }
+    res = client.get_inventory_material(search)
+    if len(res) < 1: 
+        assert False
+    for material in res:
+        if "4-Fluoro" not in material["description"]:
+            assert False
+    assert True
+
+def test_get_istartwith(client):
+    search={
+        "description__istartswith": "4-FLUORO"
+        }
+    res = client.get_inventory_material(search)
+    if len(res) < 1: 
+        assert False
+    for material in res:
+        if "4-Fluoro" not in material["description"]:
+            assert False
+    assert True
+
+def test_get_endwith(client):
+    search={
+        "description__endswith": "iodide"
+        }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if not material["description"].endswith("iodide"):
+            assert False
+    assert True
+
+def test_get_iendwith(client):
+    search={
+        "description__iendswith": "ioDIDe"
+        }
+    res = client.get_inventory_material(search)
+    for material in res:
+        if not material["description"].endswith("iodide"):
+            assert False
+    assert True
